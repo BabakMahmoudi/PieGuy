cbe49468b4dff9b12efe4cbd997afedd0e416802
pariashe <paria.mahmoudi@gmail.com>
2015-09-26 17:48:27 +0330

    adding bot

---
+++ pieGuyBot/app.js
@@ -0,0 +1,52 @@
+﻿var telegramBot = require('node-telegram-bot-api');
+var fs = require("fs");
+var pi = fs.readFileSync("pi.txt", "ascii");
+var token = ('101950127:AAGU_prhyZSSKwhQQb1eesAdNfANcSaxp4s');
+var bot = new telegramBot(token, { polling: true });
+bot.getMe().then(function (me) {
+    console.log('hi my name is %s!', me.username);
+});
+bot.on('text', function (msg) {
+    console.log(msg.from.first_name + " " + msg.from.last_name + msg.text);
+    
+    var cmd = msg.text;
+    var arg = "";
+    if (cmd.indexOf(" ") > 1) {
+        
+        cmd = msg.text.substr(0, msg.text.indexOf(" "));
+        arg = msg.text.substr(msg.text.indexOf(" ") + 1, msg.text.length - msg.text.indexOf(" "));
+        console.log("cmd='" + cmd + "'" + " arg='" + arg + "'");
+        
+    }
+    var chatId = msg.chat.id;
+    switch (cmd) {
+        case "/pi":
+            bot.sendMessage(chatId, x);
+            break;
+        case "/p":
+            bot.sendPhoto(chatId, "piPic.png");
+            break;
+        case "/search":
+            
+            if (pi.indexOf(arg) > 0) {
+                bot.sendMessage(chatId, "found: " + pi.indexOf(arg));
+                var from = pi.indexOf(arg) - 5;
+                if (from < 0) {
+                    from = 0;
+                }
+                var slice = pi.substr(from , 15);
+                bot.sendMessage(chatId, slice + "\r\n______x");
+                console.log(slice);
+            }
+            else {
+                bot.sendMessage(chatId, "not found!");
+            }
+            break;
+
+        default :
+            bot.sendMessage(chatId, "hi " + msg.from.first_name + " " + msg.from.last_name);
+            break;
+       
+    }    ;
+});
+
